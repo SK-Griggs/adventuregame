8cb8ab0a21035ec6490ff15bb39bdbd1823c4bbb
Starr K Griggs <starr.k.griggs@gmail.com>
2022-03-24 19:23:03 -0400

    game is finished. look for quirks

--- adventure.py
+++ adventure.py
@@ -44,8 +44,6 @@ while game_start =="yes":
         print("Your life is going to be pretty boring without a good book to read.")
     answer_4 = input("yes or no? ")
     
-  #If I can't get the dataframe to only display if they say yes, put it here. 
-    
     if answer_4 == "yes":      
        print(df) 
        print("These are some good titles. I need to ask a few more questions though. Do you want to read a fantasy?")     
@@ -71,7 +69,29 @@ while game_start =="yes":
     if answer_7 =="yes":
         print(df.audience)
         print(first_name, "You are in luck! That narrows it down to The Blood Trials by N.E. Davenport. ")
+        print("The Blood Trials is 2022 release, and it's about a girl who has been hiding her magic. When her grandfather dies she wants revenge and answers. She enters into  trials that will risk her secrets being uncovered. ")
+        print(df.loc[[3]])
     else:
         print("Do you want to read a middle grade book?")
+    answer_8 = input("yes or no?")
+    
+    if answer_8 =="yes":
+        print(first_name, "Another good choice! I have one middle grade fantasy for you. It's Pilar Ramirez and the Escape From Zara by Julian Randall. ")
+        print("Pilar Ramirez is about a young girl who is sucked into a magical world and discovers her missing cousin.")
+        print(df.loc[[6]])    
+    else:
+        print("Do you want to read a young adult book?")
+    answer_9 = input("yes or no? ")
 
-        game_start = input("Do you want me to help pick your next book?") 
+    if answer_9 =="yes":
+        print(first_name, "I've narrowed it down to three titles, and honestly I think you'll enjoy them all. ")
+        print(df.loc[[1]]) 
+        print("Not Even Bones is about a girl who is forced to face the monster inside of herself. ")
+        print(df.loc[[2]]) 
+        print("She Who Rides the Storm is about 4 teens, a heist and a cursed shapeshifting sword.")
+        print(df.loc[[13]])
+        print("Only a Monster is a 2022 release about a girl who discovers that she comes from a family of monsters and the boy she's been crushing on is a monster slayer. ")
+        print("She is then forced to work with a rival monster house to take down Nick.") 
+    else:
+        print(first_name, "I don't think you know what you're in the mood for. Maybe you should just take a reading break and come back when you're refreshed. ")
+    game_start = input("Do you want me to help pick your next book?") 
